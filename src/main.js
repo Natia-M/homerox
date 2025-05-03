7af0635ce5b8be0a2e9154fc8dc1c4d88c74c83b
NatiaMinasiani <natiaminasiani@gmail.com>
2025-05-03 18:50:45 +0300

    changed styles

--- src/main.js
+++ src/main.js
@@ -11,6 +11,17 @@ er = new Swiper(".mySwiper", {
   speed: 600,
   spaceBetween: 20,
   slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+    1400: {
+      slidesPerView: 3,
+    },
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
