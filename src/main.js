e82efee77f23ca6e40e3b5a4455fd93480805e79
NatiaMinasiani <natiaminasiani@gmail.com>
2025-05-25 01:35:24 +0300

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,32 +1,32 @@
 //super vip slider//
-// er = new Swiper(".mySwiper", {
-//   loop: true,
-//   centeredSlides: true,
-//   padding: "5rem",
-//   autoplay: {
-//     delay: 2000,
-//     disableOnInteraction: false,
-//     pauseOnMouseEnter: true,
-//   },
-//   speed: 600,
-//   spaceBetween: 20,
-//   slidesPerView: 1,
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 1,
-//     },
-//     1024: {
-//       slidesPerView: 4,
-//     },
-//     1400: {
-//       slidesPerView: 4,
-//     },
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
+er = new Swiper(".mySwiper", {
+  loop: true,
+  centeredSlides: true,
+  padding: "5rem",
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+  speed: 600,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 1,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+    1400: {
+      slidesPerView: 4,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 // vip slider//
 // მთავარი swiper ბოქსებისთვის
 new Swiper(".myPropertiesSwiper", {
