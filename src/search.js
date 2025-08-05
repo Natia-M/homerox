0d22d8f5f5c56652f59c6c68253aeeed48806e14
NatiaMinasiani <natiaminasiani@gmail.com>
2025-08-05 20:45:37 +0400

    style and site structure changes

--- src/search.js
+++ src/search.js
@@ -158,3 +158,18 @@ buttons.forEach((button) => {
     e.target.classList.add("active");
   });
 });
+// მხოლოდ ერთი ჩექბოქსი შეიძლებოდეს მონიშნული
+const checkbox1 = document.getElementById("checkbox1");
+const checkbox2 = document.getElementById("checkbox2");
+
+checkbox1.addEventListener("change", () => {
+  if (checkbox1.checked) {
+    checkbox2.checked = false;
+  }
+});
+
+checkbox2.addEventListener("change", () => {
+  if (checkbox2.checked) {
+    checkbox1.checked = false;
+  }
+});
